9cd128c29c0d267495202046157722218a36296a
Tavis Weeden <tavisweeden@gmail.com>
2019-09-29 19:25:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   counter = 0
+  new_array = []
   while array[counter] do
-    array[counter]**2  
+    new_array = array[counter]**2  
     counter += 1
   end
+    new_array
 end
